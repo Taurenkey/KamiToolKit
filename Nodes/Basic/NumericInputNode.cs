4d913a63ec00a35d929131a5f576f7997b909235
Taurenkey <github@taurenkey.co.uk>
2026-04-21 17:09:27 +0100

    Add TextButtonNodeSynth

--- Nodes/Basic/NumericInputNode.cs
+++ Nodes/Basic/NumericInputNode.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Numerics;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
@@ -21,7 +21,7 @@ public unsafe class NumericInputNode : ComponentNode<AtkComponentNumericInput, A
 
         BackgroundNode = new SimpleNineGridNode {
             NodeId = 8,
-            Position = new Vector2(0.0f, 3.0f),
+            Position = new Vector2(0.0f, 2.0f),
             TexturePath = "ui/uld/NumericStepperB.tex",
             TextureCoordinates = new Vector2(56.0f, 0.0f),
             TextureSize = new Vector2(24.0f, 24.0f),
